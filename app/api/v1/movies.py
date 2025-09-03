d9e27b16aeb01b33895835a4d7fa673512b0bc71
Dimitris Tagkalos <dtagkalos@gmail.com>
2025-09-03 16:53:39 +0300

    adding get endpoint

--- app/api/v1/movies.py
+++ app/api/v1/movies.py
@@ -1,13 +1,20 @@
 from fastapi import APIRouter
 
 from app.dependencies import MOVIE_DEPENDENCY
-from app.schemas.movies import MovieIn
+from app.schemas.movies import MovieIn, MovieOut
 from app.services.movies_service import MoviesService
 
-router = APIRouter()
+router = APIRouter(tags=["Movie"])
 
 
-@router.post("/movies", response_model=MovieIn, tags=["Movie"])
+@router.post("/movies", response_model=MovieIn)
 def store_movie(payload: MovieIn, service: MoviesService = MOVIE_DEPENDENCY) -> MovieIn:
     service.save(payload)
     return payload
+
+
+@router.get("/movies", response_model=list[MovieOut])
+def retrieve_movies(
+    page: int = 0, limit: int = 25, service: MoviesService = MOVIE_DEPENDENCY
+) -> list[MovieOut]:
+    return list()
--- app/schemas/movies.py
+++ app/schemas/movies.py
@@ -1,7 +1,15 @@
 from pydantic import BaseModel
 
 
-class MovieIn(BaseModel):
+class BaseMovie(BaseModel):
     title: str
     description: str
     release_year: int
+
+
+class MovieIn(BaseMovie):
+    """Movie in schema"""
+
+
+class MovieOut(BaseMovie):
+    id: str
